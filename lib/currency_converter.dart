2539b4909aa5c3b8b9f1fa625481121a25fe4545
SaiEswar15 <saieswarkumar@gmail.com>
2024-01-18 15:41:29 +0530

    creating a Stateful Widget

--- lib/currency_converter.dart
+++ lib/currency_converter.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
-class CurrencyConverter extends StatelessWidget {
 
-  
-
+class CurrencyConverter extends StatefulWidget
+{
   const CurrencyConverter({super.key});
+
+  @override
+  State<CurrencyConverter> createState() => _CurrencyConverterState();
+}
+
+class _CurrencyConverterState extends State<CurrencyConverter>
+{
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return const Text("eswar");
+  }
+}
+
+
+
+
+class CurrencyConverterr extends StatelessWidget {
+
+  const CurrencyConverterr({super.key});
 
   @override
   Widget build(BuildContext context) {
