7499f13b80a10bb2b0c6b79dd556aae1c30bdcd4
SaiEswar15 <saieswarkumar@gmail.com>
2024-01-19 23:38:57 +0530

    weather app card2 created

--- lib/Weather_App/main_screen_weather.dart
+++ lib/Weather_App/main_screen_weather.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+import "./hourly_forecast_weather.dart";
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -5,58 +7,81 @@ class WeatherScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
-      child:  Column(
-        children : [
-           SizedBox(
-            width : double.infinity,
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: double.infinity,
             child: Card(
-              
-              elevation: 10,
-              child : Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Text(
-                      "300°K",
-                      style : TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              elevation: 5,
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+                child: const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "300 ° K",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height : 10,
-                    ),
-                    Icon(
-                      Icons.cloud,
-                      size: 50,
-                    ),
-                    SizedBox(
-                      height : 10,
-                    ),
-                    Text(
-                      "Rain",
-                      style : TextStyle(
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold,
+                      SizedBox(
+                        height: 10,
                       ),
-                    ),
-                  ],
+                      Icon(
+                        Icons.cloud,
+                        size: 50,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Rain",
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-          SizedBox(
-            height : 20,
+          const SizedBox(
+            height: 20,
           ),
-          Placeholder(
-            fallbackHeight: 150,
+          const Text("Weather Forecast",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+
+          //card2
+
+          const SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                
+                HourlyForcastWidget(),
+                HourlyForcastWidget(),
+                HourlyForcastWidget(),
+                HourlyForcastWidget(),
+                HourlyForcastWidget(),
+                HourlyForcastWidget(),
+               
+              ],
+            ),
           ),
-          SizedBox(
-            height : 20,
+          const SizedBox(
+            height: 20,
           ),
-          Placeholder(
+          const Placeholder(
             fallbackHeight: 150,
           ),
         ],
@@ -64,3 +89,6 @@ class WeatherScreen extends StatelessWidget {
     );
   }
 }
+
+
+
