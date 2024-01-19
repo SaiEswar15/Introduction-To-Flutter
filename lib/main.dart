2d9d5f1cf505aaf1a4f0225f53ab4a0c6669c6f8
SaiEswar15 <saieswarkumar@gmail.com>
2024-01-19 17:51:31 +0530

    creating weather app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,68 @@
-import "package:flutter/cupertino.dart";
-import "./currency_converter_cupertino_page.dart";
+import "package:flutter/material.dart";
+import "./Weather_App/main_screen_weather.dart";
 
 void main() {
-  runApp(const MyApp()); //const is a compile time mutable
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
+  //Instead of writing the whole app inside a single runApp()
+  //we can break the code into modules so we create a class [MyApp] which
+  //extends the Stateless Widget. We will break the code into components
+  //and have them in our [MyApp] like AppBar, Card1 and Card2
 
+  @override
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context)
-  {
-    return const CupertinoApp(
-      home : CurrencyConverterCupertinoPage()
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+
+          centerTitle: true,
+          title: const Text(
+            "Weather App",
+            style: TextStyle(
+              
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+
+          actions: [
+            IconButton(
+              onPressed: (){
+                debugPrint("refreshed");
+              },
+              icon: const Icon(
+                Icons.refresh
+              ),
+            ),
+            // GestureDetector(
+            //   onTap: (){
+            //     debugPrint("refreshed");
+            //   },
+            //   child: const Icon(
+            //     Icons.refresh,
+            //   ),
+            // ),
+            // InkWell(
+            //   onTap: (){
+            //     debugPrint("refreshed");
+            //   },
+            //   child: const Icon(
+            //     Icons.refresh,
+            //   ),
+            // ),
+          ],
+        ),
+        body: const WeatherScreen(),
+      ),
     );
   }
 }
