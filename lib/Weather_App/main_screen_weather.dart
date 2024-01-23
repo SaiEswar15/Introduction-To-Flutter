af2ee36cff78f57ab08f8ee4443ecec977e3c4bf
SaiEswar15 <saieswarkumar@gmail.com>
2024-01-23 17:06:25 +0530

    card3 created

--- lib/Weather_App/main_screen_weather.dart
+++ lib/Weather_App/main_screen_weather.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'package:helloworld/Weather_App/additional_forecast_weather.dart';
 import "./hourly_forecast_weather.dart";
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,7 @@ class WeatherScreen extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          //card1
           SizedBox(
             width: double.infinity,
             child: Card(
@@ -55,40 +57,65 @@ class WeatherScreen extends StatelessWidget {
               ),
             ),
           ),
+
           const SizedBox(
             height: 20,
           ),
+
           const Text("Weather Forecast",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
 
           //card2
-
           const SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Row(
               children: [
-                
-                HourlyForcastWidget(),
-                HourlyForcastWidget(),
-                HourlyForcastWidget(),
-                HourlyForcastWidget(),
-                HourlyForcastWidget(),
-                HourlyForcastWidget(),
-               
+                HourlyForcastWidget(
+                    time: "09 : 00", icon: Icons.cloud, temperature: "301.17"),
+                HourlyForcastWidget(
+                    time: "12 : 00", icon: Icons.cloud, temperature: "301.54"),
+                HourlyForcastWidget(
+                    time: "15 : 00", icon: Icons.cloud, temperature: "301.11"),
+                HourlyForcastWidget(
+                    time: "18 : 00", icon: Icons.cloud, temperature: "300.75"),
+                HourlyForcastWidget(
+                    time: "20 : 00", icon: Icons.cloud, temperature: "301.81"),
+                HourlyForcastWidget(
+                    time: "23 : 00", icon: Icons.cloud, temperature: "298.67"),
               ],
             ),
           ),
+
           const SizedBox(
             height: 20,
           ),
-          const Placeholder(
-            fallbackHeight: 150,
+
+          //card3
+          const Text(
+            "Additional Information",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              AdditionalForecast(icon : Icons.water_drop, type : "Humidity", value : "94"),
+              AdditionalForecast(icon : Icons.wind_power, type : "Wind Speed", value : "7.67"),
+              AdditionalForecast(icon : Icons.beach_access, type : "Pressure", value : "1006"),
+            ],
+          ),
+
+          const SizedBox(
+            height: 20,
           ),
         ],
       ),
+      
     );
   }
 }
-
 
 
