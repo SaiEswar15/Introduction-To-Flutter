66a94508263d89290ec2fb49eb66bc47b7215b0b
SaiEswar15 <saieswarkumar@gmail.com>
2024-01-25 22:22:12 +0530

    refresh button, setState

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-
 import "package:flutter/material.dart";
 import "./Weather_App/main_screen_weather.dart";
-
-// import "Weather_App/secrets.dart";
-//api.openweathermap.org/data/2.5/weather?q=$cityName&APPID=$weatherApiKey
 
 void main() {
   runApp(const MyApp());
@@ -21,48 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark(
         useMaterial3: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-
-          centerTitle: true,
-          title: const Text(
-            "Weather App",
-            style: TextStyle(
-              
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
-          ),
-
-          actions: [
-            IconButton(
-              onPressed: (){
-                debugPrint("refreshed");
-              },
-              icon: const Icon(
-                Icons.refresh
-              ),
-            ),
-            // GestureDetector(
-            //   onTap: (){
-            //     debugPrint("refreshed");
-            //   },
-            //   child: const Icon(
-            //     Icons.refresh,
-            //   ),
-            // ),
-            // InkWell(
-            //   onTap: (){
-            //     debugPrint("refreshed");
-            //   },
-            //   child: const Icon(
-            //     Icons.refresh,
-            //   ),
-            // ),
-          ],
-        ),
-        body: const WeatherScreen(),
-      ),
+      home: const WeatherScreen(),
     );
   }
 }
