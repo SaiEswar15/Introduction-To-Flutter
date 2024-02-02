a5162b0139d7754126f457d58da87c440d764824
SaiEswar15 <saieswarkumar@gmail.com>
2024-02-02 15:23:00 +0530

    brand categories bar created

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
+  final List<String> list = const ["All", "Nike", "Adidas", "Puma", "Bata"];
+
   @override
   Widget build(BuildContext context) {
-    
     const textborder = OutlineInputBorder(
       borderSide: BorderSide(),
       borderRadius: BorderRadius.horizontal(
@@ -13,33 +14,76 @@ class Homepage extends StatelessWidget {
       ),
     );
 
-    return const Scaffold(
+    const borderedRadius = ShapeDecoration(
+      color: Colors.blueAccent,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.horizontal(
+          left: Radius.circular(20),
+          right: Radius.circular(20),
+        ),
+      ),
+    );
+
+    return Scaffold(
       body: SafeArea(
-          child: Row(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              "Shoes\nCollections",
-              style: TextStyle(
-                fontSize: 25,
-              ),
-            ),
-          ),
-          Expanded(
-            child: TextField(
-              decoration: InputDecoration(
-                border: textborder,
-                enabledBorder: textborder,
-                prefixIcon: Icon(
-                  Icons.search,
+        child: Column(
+          children: [
+            const Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    "Shoes\nCollections",
+                    style: TextStyle(
+                      fontSize: 25,
+                    ),
+                  ),
                 ),
-                hintText: "Search",
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: textborder,
+                      enabledBorder: textborder,
+                      prefixIcon: Icon(
+                        Icons.search,
+                      ),
+                      hintText: "Search",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ColoredBox(
+              color: const Color.fromARGB(255, 255, 255, 255),
+              child: SizedBox(
+                height: 50,
+                width: 600,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: list.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration : borderedRadius,
+                        width: 70,
+                        child: Center(
+                          child: Text(
+                            list[index],
+                            style: const TextStyle(
+                              color : Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-        ],
-      )),
+          ],
+        ),
+      ),
     );
   }
 }
