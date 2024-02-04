138a04c1fdc25059373c703d8e3863b4e1c87e50
SaiEswar15 <saieswarkumar@gmail.com>
2024-02-04 12:12:42 +0530

    creating a cart page

---
+++ lib/CartPage/cartpage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title : const Text(
+          "cart",
+        ),
+      ),
+      body : const Center(
+        child: Text(
+          "welcome to cart page"
+        ),
+      ),
+    );
+  }
+}
